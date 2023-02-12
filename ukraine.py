3f9ac296a6fdf9d20645750689960a1887e820e5
BazilevychIevgen <barocko2005@ukr.net>
2023-02-12 13:42:27 +0100

    second

--- ukraine.py
+++ ukraine.py
@@ -18,7 +18,7 @@ class Database:
        return self.__database_name
 
    def set_database_name(self , value):
-       self.__database_name = value dsaewq
+       self.__database_name = value git st
        
 
     
